e50062dcd416c550b57727ba36656a2d43da699b
Oleksii Borodenko <kosunix@gmail.com>
2015-09-08 14:46:56 +0300

    Initial commit. Implementation algorithms defining unique characters in string. 1) compare each element with all; 2) based on boolean char set; 3) compare element with neighbor in sorted array; 4) using extension methods;

---
+++ Algorithms.Core.Tests/StringTests.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms.Core.Tests
+{
+    [TestFixture]
+    public class StringTests
+    {
+        /// <summary>
+        /// ASCII - 256 (0 - 255) characters
+        /// UTF16 - 65533 characters
+        /// </summary>
+        [TestCase("feljxiu")]
+        [TestCase("9ik0;l")]
+        [TestCase("f4iu")]
+        [TestCase("rituo")]
+        public void UniqueCharacters1True(string value)
+        {
+            var result = String.IsUniqueCharacters1(value);
+
+            Assert.That(result, Is.True);
+        }
+
+        [TestCase("rituoi")]
+        public void UniqueCharacters1False(string value)
+        {
+            var result = String.IsUniqueCharacters1(value);
+
+            Assert.That(result, Is.False);
+        }
+
+        [TestCase("feljxiu")]
+        [TestCase("9ik0;l")]
+        [TestCase("f4iu")]
+        [TestCase("rituo")]
+        public void UniqueCharacters2True(string value)
+        {
+            var result = String.IsUniqueCharacters2(value);
+
+            Assert.That(result, Is.True);
+        }
+
+        [TestCase("rituoi")]
+        public void UniqueCharacters2False(string value)
+        {
+            var result = String.IsUniqueCharacters2(value);
+
+            Assert.That(result, Is.False);
+        }
+
+        [TestCase("feljxiu")]
+        [TestCase("9ik0;l")]
+        [TestCase("f4iu")]
+        [TestCase("rituo")]
+        public void UniqueCharacters3True(string value)
+        {
+            var result = String.IsUniqueCharacters3(value);
+
+            Assert.That(result, Is.True);
+        }
+
+        [TestCase("rituoi")]
+        public void UniqueCharacters3False(string value)
+        {
+            var result = String.IsUniqueCharacters3(value);
+
+            Assert.That(result, Is.False);
+        }
+
+        [TestCase("feljxiu")]
+        [TestCase("9ik0;l")]
+        [TestCase("f4iu")]
+        [TestCase("rituo")]
+        public void UniqueCharacters4True(string value)
+        {
+            var result = String.IsUniqueCharacters4(value);
+
+            Assert.That(result, Is.True);
+        }
+
+        [TestCase("rituoi")]
+        public void UniqueCharacters4False(string value)
+        {
+            var result = String.IsUniqueCharacters4(value);
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
---
+++ Algorithms.Core/String.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+
+namespace Algorithms.Core
+{
+    using System;
+
+    /// <summary>
+    /// The string.
+    /// </summary>
+    public static class String
+    {
+        /// <summary>
+        /// Time complexity - O(n*n), where n - length of the string
+        /// Space complexity - O(1)
+        /// </summary>
+        public static bool IsUniqueCharacters1(string value)
+        {
+            var length = value.Length;
+
+            for (var i = 0; i < length; i++)
+            {
+                for (var j = 0; j < length; j++)
+                {
+                    if (i == j)
+                        continue;
+
+                    if (value.ElementAt(i) == value.ElementAt(j))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Time complexity - O(n), where n - length of the string
+        /// Space complexity - O(1)
+        /// </summary>
+        public static bool IsUniqueCharacters2(string value)
+        {
+            if (value.Length > 256)
+                return false;
+
+            var charSet = new bool[256];
+            for (var i = 0; i < value.Length; i++)
+            {
+                int val = value.ElementAt(i);
+
+                if (charSet[val])
+                {
+                    return false;
+                }
+
+                charSet[val] = true;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Time complexity - O(n * log(n))
+        /// </summary>
+        public static bool IsUniqueCharacters3(string value)
+        {
+            var sortedString = value.ToCharArray();
+            Array.Sort(sortedString);
+
+            for (var i = 0; i < sortedString.Length - 1; i++)
+            {
+                if (sortedString[i] == sortedString[i + 1])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsUniqueCharacters4(string value)
+        {
+            return !value.GroupBy(c => c).Any(g => g.Count() > 1);
+        }
+    }
+}
