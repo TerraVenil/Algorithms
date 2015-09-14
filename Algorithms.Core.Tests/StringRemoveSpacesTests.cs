453b7adccb26e7418a95e9933ad1cca72ca52756
Oleksii Borodenko <kosunix@gmail.com>
2015-09-14 14:05:33 +0300

    Implementation algorithms removing spaces from string. 1) using build in method Trim, Replace; 2) using StringBuilder, StringReader; 3) based on regular expressions;

---
+++ Algorithms.Core.Tests/StringRemoveSpacesTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms.Core.Tests
+{
+    public class StringRemoveSpacesTests
+    {
+        [TestCase("Mr John Smith               ")]
+        [TestCase("               Mr John Smith")]
+        public void RemoveSpaces1(string value)
+        {
+            var result = String.RemoveSpaces1(value);
+
+            Assert.That(result, Is.EqualTo("Mr%20John%20Smith"));
+        }
+
+        [TestCase("Mr John Smith               ")]
+        [TestCase("               Mr John Smith")]
+        public void RemoveSpaces2(string value)
+        {
+            var result = String.RemoveSpaces2(value, 13);
+
+            Assert.That(result, Is.EqualTo("Mr%20John%20Smith"));
+        }
+
+        [TestCase("Mr John Smith               ")]
+        [TestCase("               Mr John Smith")]
+        public void RemoveSpaces3(string value)
+        {
+            var result = String.RemoveSpaces3(value);
+
+            Assert.That(result, Is.EqualTo("Mr%20John%20Smith"));
+        }
+
+        [Test]
+        public void RemoveSpaces4()
+        {
+            var result = String.RemoveSpaces4("Mr John Smith               ", 13);
+
+            Assert.That(result, Is.EqualTo("Mr%20John%20Smith"));
+        }
+    }
+}
---
+++ Algorithms.Core/StringRemoveSpaces.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Algorithms.Core
+{
+    public static partial class String
+    {
+        public static string RemoveSpaces1(string value)
+        {
+            return value.Trim().Replace(" ", "%20");
+        }
+
+        public static string RemoveSpaces2(string value, int length)
+        {
+            var stringBuilder = new StringBuilder();
+            using (var stringReader = new StringReader(value))
+            {
+                while (stringReader.Peek() != -1)
+                {
+                    var ch = (char)stringReader.Peek();
+                    if (char.IsWhiteSpace(ch))
+                    {
+                        while (char.IsWhiteSpace(ch))
+                        {
+                            stringReader.Read();
+                            ch = (char)stringReader.Peek();
+                        }
+
+                        if (stringBuilder.Length != 0 && stringBuilder.Length < length)
+                        {
+                            stringBuilder.Append("%20");
+                        }
+                    }
+                    else
+                    {
+                        stringBuilder.Append((char)stringReader.Read());
+                    }
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static string RemoveSpaces3(string str)
+        {
+            var reg1 = Regex.Replace(str, @"^\s+|\s+$", string.Empty);
+            return Regex.Replace(reg1, @"\s+", "%20");
+        }
+
+        public static string RemoveSpaces4(string value, int length)
+        {
+            var modifiedString = string.Empty;
+            for (var i = 0; i < length; i++)
+            {
+                if (char.IsWhiteSpace(value[i]))
+                {
+                    modifiedString += "%20";
+                }
+                else
+                {
+                    modifiedString += value[i];
+                }
+            }
+
+            return modifiedString;
+        }
+    }
+}
