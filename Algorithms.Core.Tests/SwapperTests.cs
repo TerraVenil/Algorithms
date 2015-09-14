c18b2b034734c8bc5a66f97c441ba0a15cbec7fa
Oleksii Borodenko <kosunix@gmail.com>
2015-09-14 13:59:17 +0300

    Added xUnit library. Implementation XOR swap algorithm.

---
+++ Algorithms.Core.Tests/SwapperTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Algorithms.Core.Tests
+{
+    public class SwapperTests
+    {
+        [Fact]
+        public void SwapTwoChars()
+        {
+            var ab = new[] { 'a', 'b' };
+
+            Swapper.Swap(ref ab[0], ref ab[1]);
+
+            Assert.Equal('b', ab[0]);
+            Assert.Equal('a', ab[1]);
+        }
+    }
+}
---
+++ Algorithms.Core/Swapper.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms.Core
+{
+    public class Swapper
+    {
+        /// <summary>
+        /// XOR swap algorithm - https://en.wikipedia.org/wiki/XOR_swap_algorithm
+        /// </summary>
+        public static void Swap(ref char x, ref char y)
+        {
+            if (x == y)
+            {
+                return;
+            }
+
+            x ^= y;
+            y ^= x;
+            x ^= y;
+        }
+    }
+}
